cd1b71f0a743da4b8b28f7bab7c01c06d43ff14e
Eddy Salzmann <hello@eddysalzmann.com>
2020-12-02 14:47:23 +0100

    Initial Commit

---
+++ Metronome Audiokit Test/Metronome_Audiokit_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metronome_Audiokit_TestApp.swift
+//  Metronome Audiokit Test
+//
+//  Created by Eddy Salzmann on 02.12.20.
+//
+
+import SwiftUI
+
+@main
+struct Metronome_Audiokit_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
